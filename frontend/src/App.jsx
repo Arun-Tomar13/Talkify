826f2668e92b6a2cd541d5f6a323627bfe048581
Arun Tomar <aruntomar1330@gmail.com>
2025-08-28 12:45:10 +0530

    Add Zustand for theme management and implement theme selector component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,18 +10,20 @@ import  {Toaster} from 'react-hot-toast';
 import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.js";
 import Layout from "./components/Layout.jsx";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 
 function App() {
   
   const {isLoading,authUser} = useAuthUser();
+  const {theme,setTheme} = useThemeStore();
 
   const isAuthenticated =  Boolean(authUser);
   const isOnboarded = authUser?.isOnborded;
 
   if(isLoading) return <PageLoader/>
   return (
-    <div className=' h-screen' data-theme="night">
+    <div className=' h-screen' data-theme={theme}>
      <Routes>
         <Route path="/" element={
           isAuthenticated && isOnboarded ? 
